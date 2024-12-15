ae4882e797f706c6bafbf71fa719d399fe64700c
Aishwary001 <harshrajaparmar17@gmail.com>
2024-12-16 01:37:12 +0530

    Basic database is ready for understanding

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const connectToDb = require('./DB/db');
+connectToDb();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Welcome maa");
--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,5 @@
 const http = require('http');
 const app = require('./app');
-const dotenv = require('dotenv');
-dotenv.config();
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
